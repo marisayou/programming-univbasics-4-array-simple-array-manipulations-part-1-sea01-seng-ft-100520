35ea6c80f20b75bfa3a16f833d97b622f44b4726
Marisa You <marisa.you@gmail.com>
2020-08-28 05:29:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_push (array, string)
   return array.push(string)
 end
 
-def using_unshift (array)
-  return array.shift
+def using_unshift (array, string)
+  return array.unshift(string)
 end
 
 def using_pop (array)
